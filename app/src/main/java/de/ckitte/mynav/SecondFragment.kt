b9a05284922da57304e819d4c6a9890f7f884126
Christian Kitte <christian.kitte@stud.hs-emden-leer.de>
2021-04-22 16:11:18 +0200

    Initial commit

---
+++ app/src/main/java/de/ckitte/mynav/SecondFragment.kt
@@ -0,0 +1,34 @@
+package de.ckitte.mynav
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SecondFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.btnButton2).setOnClickListener({
+            findNavController().navigate(R.id.action_SecondFragement_to_FirstFragement)
+        })
+    }
+}
